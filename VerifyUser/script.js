735de275bd607c4257f3fb9f6d078fc2eeb2ac56
Raj Kalyan Kumar <135142832+KalyanKodes@users.noreply.github.com>
2023-09-30 16:18:46 +0530

    Add files via upload

---
+++ VerifyUser/script.js
@@ -0,0 +1,25 @@
+const codes = document.querySelectorAll('.code');
+codes[0].focus();
+
+codes.forEach((code, idx) => {
+  code.addEventListener('keydown', (e) => {
+    if (e.key >= '0' && e.key <= '9') {
+      codes[idx].value = '';
+      setTimeout(() => codes[idx + 1].focus(), 10);
+    } else if (e.key === 'Backspace') {
+      setTimeout(() => codes[idx - 1].focus(), 10);
+    }
+  });
+});
+
+
+document.querySelector('.vefiryButton').addEventListener('click' , ()=>{
+    let status = codes[4].value != ""?true:false;
+
+    if(status){
+        alert("User Verified Sucessfully")
+    }
+    else{
+        alert("User Not Verified")
+    }
+})
